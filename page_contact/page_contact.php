165e26e802560467745ff3e6391e2223eb7d5ea4
sp6men <37633117+sp6men@users.noreply.github.com>
2018-07-21 13:09:42 +0200

    Add files via upload

---
+++ page_contact/page_contact.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+ <html>
+     <head>
+     
+     <meta charset="utf-8">
+     <meta name="viewport" content="">
+         <link rel="stylesheet" type="text/css" href="stylecontact.css">
+         <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Cinzel|Noticia+Text" rel="stylesheet"> 
+     
+</head>
+
+
+    <body>
+        
+          <div class="navbar">
+                
+             <a class="active" href="#">CONTACT</a>
+                <a href="#">SERVICES</a>
+                  <a href="#">PRODUITS</a>
+                    <a href="#">ACCUEIL</a>
+                
+                       </div>
+        
+        <img src="background1.jpg" alt="background" class="backgroundcontact">
+        
+        <div class="textecontact">
+            
+                    <p class="suggestion">Une suggestion ?</p> 
+                    <p class="commande">Une commande spéciale ?</p>
+                    <p class="contactez">Contactez-nous ! </p>
+                
+                </div>
+
+                <p class="barre">_________________________________________________</p>
+                <div id="barre"></div> <div id="barre"></div> 
+                <p class="textform">FORMULAIRE DE CONTACT</p>
+                <p class="barre2">____________________</p>
+        
+        
+        <div class="form">
+                    <form class="">
+                       
+                        <input type="text" name="firstname" placeholder=" Votre nom" class="votrenom"><br>
+                       
+                        <input type="text" name="lastname" placeholder=" Votre E-mail" class="votremail"><br>
+                       
+                        <input type="text" name="msg" placeholder=" Votre message" class="message">
+                        </form>
+            
+                        <input type="submit" name="Envoyer"  class="boutonenvoyer">
+        </div>
+        
+        <div class="reseauxsociaux"> 
+        
+            <a href="" target="_blank" class="twitter"><img src="twitter.png" alt="twitter" class="twitter"></a>
+            <a href="" target="_blank" class="facebook"><img src="facebook.png" alt="facebook" class="facebook"></a>
+            <a href="" target="_blank" class="instagram"><img src="instagram.png" alt="instagram" class="instagram"></a>
+            
+            
+                        </div>
+        
+     </body>
+</html>
