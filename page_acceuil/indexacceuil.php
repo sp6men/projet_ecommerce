ea23c593b3f6617ba2802b92454131363cda0ab9
sp6men <37633117+sp6men@users.noreply.github.com>
2018-07-21 13:13:01 +0200

    Add files via upload

---
+++ page_acceuil/indexacceuil.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+ <html>
+     <head>
+     
+     <meta charset="utf-8">
+         
+         <meta name="viewport" content="">
+         <link rel="stylesheet" type="text/css" href="styleacceuil.css">
+         <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Cinzel|Noticia+Text" rel="stylesheet"> 
+          
+     
+</head>
+
+
+<body>
+        
+     
+     <p class="descriptionaccueil">Vente de fromages locaux européens</p>
+    <p class="barre">___________</p>
+    <div id="barre"></div> <div id="barre"></div> 
+    <div class="texteaccueil">
+
+    <h1 class="fromagerie">Fromagerie</h1>
+      <h1 class="simplon">Simplon</h1>
+       
+             <p class="boutonentrertexte">
+                 <a class="boutonentrer" href="/projet_ecommerce/index/index.php">Commencer</a>
+                    </p>
+        </div>
+
+     <div class="navbaraccueil">
+                
+             <a class="active" href="#">CONTACT</a>
+                <a href="#">SERVICES</a>
+                  <a href="#">PRODUITS</a>
+                    <a href="#">ACCUEIL</a>
+                
+                       </div>
+       <img src="background1.jpg" alt="backgroundimage" class="imageaccueil">
+            
+
+     
+     </body>
+
+</html>
